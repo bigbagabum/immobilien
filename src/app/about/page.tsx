7b75645ac2497a28eab438d04736566c3c38e69d
Dmitrii Evdokimov <evdokimovbest@gmail.com>
2026-01-22 09:51:15 +0100

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function About() {
+    return (
+      <section>
+        <h2>About</h2>
+        <Link href='/'>Back to home</Link>
+      </section>
+    );
+  }
---
+++ src/app/help/page.tsx
@@ -0,0 +1,7 @@
+export default function Help() {
+    return (
+      <section>
+        <h2>FAQ</h2>
+      </section>
+    );
+  }
