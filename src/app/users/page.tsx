2a3d072c9579046619e1adddf1e6ab719e231816
Dmitrii Evdokimov <evdokimovbest@gmail.com>
2026-01-23 09:55:09 +0100

    build variants

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,13 +1,16 @@
 import { User } from "@/common/types/User";
 async function fetchUsers(): Promise<User[]> {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    //cache: "no-store",//ssa - server site rendering
+    //cache: "no-store",
     next: { revalidate: 60 }, //isr - incremental static regeneration
   });
   return res.json();
 }
-//SSG - static site generated
-//cache: "no-store"
+//SSG - 	При билде (next build)
+// next: { revalidate: false }, // или не указывать вовсе
+
+//SSR	При каждом запросе
+//cache: "no-store",
 
 //ISR - Incremental static regeneration
 //next: { revalidate: 60 }, save the page for 60 seconds
